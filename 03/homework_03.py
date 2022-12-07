df3be1a6e343010bed1717b93d7a310923e2b16b
Bilyk-VM <bilikvn@gmail.com>
2022-12-07 15:21:06 +0200

    add home work 3

---
+++ 03/homework_03.py
@@ -0,0 +1,41 @@
+
+# Task one ======================================
+phrase = 'I love Python'
+kilkist = input('Введіть кількість повторень: ')
+print(('' + phrase + '') * int(kilkist))
+print()
+
+# Task two ======================================
+age_in_month = input('Введіть свій вік в місяцях: ')
+age_in_month = int(age_in_month)
+print('Ваш вік -',  age_in_month, 'місяців ')
+
+# Task three ======================================
+age_in_years = age_in_month // 12
+print('Тобто на даний момент вам -', age_in_years, 'років ')
+print()
+
+# Task four ======================================
+my_name = input('Введіть будьласка своє імя: ')
+my_age = 'My name is ' + my_name + '. I`m ' + str(age_in_years) + ' years old.'
+print(my_age)
+print()
+
+# Task five ======================================
+zminna_1 = 1
+print('Значення першої змінної дорівнює', zminna_1)
+for i in range(1, 6):
+    zminna_2 = input('Введіть значення другої змінної: ')
+    print(zminna_1 == int(zminna_2))
+    i += i
+print()
+
+# Task six ======================================
+a = 2
+print('Змінна а =', a)
+b = 5
+print('Змінна b =', b)
+c = 6
+print('Змінна c =', c)
+d = str(a) + str(b) + str(c)
+print('По умовам задачі, змінна d дорівнює:', d)
