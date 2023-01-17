aac7dcf6c80b456e92720272dfe541cf96ffd1d4
Bilyk-VM <bilikvn@gmail.com>
2023-01-17 10:15:52 +0200

    editing homework_11

--- 11/homework_11.py
+++ 11/homework_11.py
@@ -7,33 +7,34 @@ from datetime import datetime
 def deco(func):
     def inner(*args, **kwargs):
         # виведем назву функції, за допомогою методу __name__
-        print(f"\nFunction name is: '{func.__name__}'")
+        print(f"Function name is: '{func.__name__}'")
         # виведем час виклику функції
         now = datetime.now()
         print(f"Today's date is: {now.strftime('%d.%m.%Y %H:%M')}")
         # викликаємо функцію, яка буде декоруватись
-        func(*args, **kwargs)
+        res = func(*args, **kwargs)
+        return res
     return inner
 
 # задекоруємо декілька простих функцій
 @deco
-def first_func():
-    print('This is the first function')
+def first_func(val_1):
+    print(f'This is the first function with value: {val_1}')
 
 @deco
-def second_func():
-    print('This is the second function')
+def second_func(val_2):
+    print(f'This is the second function with value: {val_2}')
 
 # викличем ці функції
-first_func()
+first_func(5)
 print()
-second_func()
+second_func(10)
 
 
 # ==================== 11.2
 # Створити кастомний Exception клас, який має повідомляти "Custom exception is occured"
 class MyCustomException(Exception):
-   print("Custom exception is occured")
+   print("\nCustom exception is occured\n")
 
 
 # ==================== 11.3
@@ -42,12 +43,13 @@ class MyCustomException_2:
     def __enter__(self):
         print('==========')
     def __exit__(self, exc_type, exc_val, exc_tb):
-        print('==========')
+        print('==========\n')
+        return True
 
 # вкладемо контекстний менеджер в try except
 try:
     with MyCustomException_2():
-        print("\nProgram code execution\n")
+        print("Program code execution")
 except Exception as e:
     print(f'Exception: {e}')
 
@@ -56,7 +58,8 @@ except Exception as e:
 # створимо конструкцію try except
 try:
     print('=' * 10)
-    print("\nProgram code execution\n")
+    print("Program code execution")
 except Exception as e:
-    print('=' * 10)
     print(f"Exception: {e}")
+finally:
+    print('=' * 10)
